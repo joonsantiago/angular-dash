356c8cf0f1f8fb1b43e1ecdd066e6b9632d22286
Jhon Santiago <santjonatas@gmail.com>
2021-01-27 20:38:56 -0300

    Iniciando layout, navbar com menu lateral

---
+++ src/app/template/template.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TemplateComponent } from './template.component';
+import { MaterialModulesModule } from '../core/material-modules/material-modules.module';
+
+@NgModule({
+  declarations: [
+    TemplateComponent
+  ],
+  imports: [
+    CommonModule,
+    MaterialModulesModule
+  ],
+  exports: [
+    TemplateComponent
+  ]
+})
+export class TemplateModule { }
